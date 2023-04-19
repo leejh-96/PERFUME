7420b8b43e6b4d9b7f79a90e2d679b53937bdbae
콩밍디 <kong032149@naver.com>
2023-04-19 15:22:52 +0900

    0419 - 썩은 커밋 2탄

--- PERFUME/src/main/java/com/scent/perfume/planning/controller/MemberController.java
+++ PERFUME/src/main/java/com/scent/perfume/planning/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.scent.perfume.planning.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
