501f28f79b37f0c3e2b34c67fa53b33305d6e61c
DESKTOP-9ONH3DQ\user1 <dljuhee@naver.com>
2023-04-25 12:45:07 +0900

    회원가입 이메일 인증 가능하게 변경

--- PERFUME/src/main/java/com/scent/perfume/event/controller/EventController.java
+++ PERFUME/src/main/java/com/scent/perfume/event/controller/EventController.java
@@ -131,8 +131,8 @@ public class EventController {
 		
 		result = service.save(member, terms, tCheck);	// tCheck 선택약관동의
 		
-		// 인증 메일 보내는 메소드
-		// mailsender.mailSendWithUserKey(member.getMail(),member.getId(), request);
+		// 인증 메일 보내는 메소드 기능 시연 시 주석 풀어두기
+		 mailsender.mailSendWithUserKey(member.getMail(),member.getId(), request);
 		
 		if(result > 0) {
 			modelAndView.addObject("msg", "회원가입 인증 이메일이 발송되었습니다. 등록한 이메일을 확인해주시고 인증 절차를 거쳐주시기 바랍니다.");
